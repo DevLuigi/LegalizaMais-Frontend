4cc39c181cec260af47fc7e8076efc0a89dc9b16
DevLuigi <luigidasilvacoelho@gmail.com>
2025-09-28 17:19:00 -0300

    feat: Adiciona exemplo do fluxo padrão da aplicação

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,7 +2,7 @@ import { Container } from "./styled.js";
 
 export default function Button(props) {
     return(
-        <Container color={props.color} onClick={() => props.onClick && props.onClick()}>
+        <Container id={props.id} color={props.color} onClick={() => props.onClick && props.onClick()}>
             {props.children}
         </Container>
     )
--- src/service/config/config.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = 'http://localhost:8080';
-
-export const Errors = {
-    General: 'GeneralError',
-    Unknown: 'UnknownError'
-}
