e83f0e4d23495fda6db76ddeae8ad448fe00626c
DevLuigi <luigidasilvacoelho@gmail.com>
2025-10-26 16:43:44 -0300

    feat: Adiciona componente de tabela e exemplo de uso

--- src/pages/budgets/list/index.jsx
+++ src/pages/budgets/list/index.jsx
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
 import ViewMain from "../../../components/view/viewMain";
 import EmptyPage from "../../../components/emptyPage";
+import Table from "../../../components/table";
+import { Container } from "./styled.js";
 
 export default function ListBudgets() {
     const [budgets, setBudgets] = useState([]);
     const path = ["Orçamentos", "Lista de orçamentos"];
 
+    const data = Array(12).fill({
+        corinthians: "Cacanabis ancião da fiel",
+        "CPF/CNPJ": "831.884.710-50",
+        endereçooooo: "Rua teste, bairro teste, nº 100",
+        telefone: "(11) 999999999",
+        email: "clienteTeste@email.com",
+        campo1: "Valor 1",
+        campo2: "Valor 2",
+    });
+
+    let actions = new Map();
+    actions.set("edit", () => { alert("Substituir por função de editar") });
+    actions.set("delete", () => { alert("Substituir por função de deletar") });
+    actions.set("PDF", () => { alert("Substituir por função de gerar PDF") });
+    actions.set("email", () => { alert("Substituir por função de enviar email") });
+    actions.set("whatsapp", () => { alert("Substituir por função de enviar WhatsApp") });
+    // actions.set("none", () => {}); // Use "none" para esconder a coluna de ações
+
     useEffect(() => {
-        setBudgets([]);
+        setBudgets(['corinthians']);
     }, []);
 
     return(
@@ -23,7 +43,7 @@ export default function ListBudgets() {
             (
                 <ViewMain path={path}>
                     <Container>
-                        
+                        <Table data={data} actions={actions} />
                     </Container>
                 </ViewMain>
             )
